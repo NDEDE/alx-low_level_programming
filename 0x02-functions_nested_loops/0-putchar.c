175d376389d73201836cebc31fac4c1bf61a379d
NDEDE <dennicendede@gmail.com>
2022-07-05 22:40:51 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Dennice Ndede
+ *
+ */
+
+#include "putchar.h"
+
+/**
+ * main -Prints "-putchar" followed by a new linee.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_puthcar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\r');
+
+	return (0);
+}
