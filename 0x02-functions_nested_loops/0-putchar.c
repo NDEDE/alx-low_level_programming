986138f2256e81ed5b33605bd98ec7ab67340bc4
NDEDE <dennicendede@gmail.com>
2022-07-05 23:29:53 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,11 @@
 #include "main.h"
 
 /**
- * main -Prints "prin_alphabet" followed by a new linee.
+ * main -Prints "print_alphabet" followed by a new linee.
  *
  * Return: Always 0.
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
