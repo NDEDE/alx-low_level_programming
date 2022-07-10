76f6b683e151322cde49c1659d027659c259af45
NDEDE <dennicendede@gmail.com>
2022-07-10 11:05:37 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
  */
 void print_alphabet(void)
 {
-    int j;
-        
-    for (j = 'a'; j <= 'z'; j++)
-    {
-	_putchar(j);
-    }
-    _putchar('\n');
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
 }
