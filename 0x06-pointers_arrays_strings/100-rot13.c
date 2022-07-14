15858c3030cc9ac9e7163790c5e471afb484e969
NDEDE <dennicendede@gmail.com>
2022-07-14 07:47:10 -0700

    m

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+/*
+ * File: 100-rot13.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encoded
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
