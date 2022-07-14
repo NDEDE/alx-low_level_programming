67512b1a63e72c67fe13cb3c389fe93a2b0d84ea
NDEDE <dennicendede@gmail.com>
2022-07-14 05:14:21 -0700

    m

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+/*
+ * File: Dennice Ndede
+ * Auth:  1-strncat.c
+ */
+
+#include "main.h"
+
+/**
+ * _strncat - appends to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
+}
