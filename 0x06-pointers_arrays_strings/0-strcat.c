ef7dfbe00cf915764491f777a708b8672da95d5d
NDEDE <dennicendede@gmail.com>
2022-07-14 04:44:55 -0700

    A function that concatenatestwo strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,19 +2,19 @@
  * File: 0-strcat.c
  * Auth: Dennice Ndede
  */
-                    
+
 #include "main.h"
-                                      
-/**                                
+
+/**
  * _strcat -appendes src to the dest string
  * @dest: string to append by src
- * @src: string to append to dest      
+ * @src: string to append to dest
  *Return: address of dest
- */                     
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	                                       
+
 	i = j = 0;
 	while (*(dest + i))
 		i++;
