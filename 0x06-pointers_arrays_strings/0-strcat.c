d5ad308b8f1101efc5571b332a3ea88d9f40c882
NDEDE <dennicendede@gmail.com>
2022-07-14 04:18:51 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,15 @@
  * File: 0-strcat.c
  * Auth: Dennice Ndede
  */
-             
+                    
+#include "main.h"
+                                      
 /**                                
  * _strcat -appendes src to the dest string
  * @dest: string to append by src
- * @src: string to append to dest
- *                                          
+ * @src: string to append to dest      
  *Return: address of dest
- */
+ */                     
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
