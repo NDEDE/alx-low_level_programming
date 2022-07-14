fd8bff0c401cb5271f387eca18b8803e5fb6afe3
NDEDE <dennicendede@gmail.com>
2022-07-14 07:11:22 -0700

    m

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+/*
+ * File: 7-leet.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * leet -Entry point
+ * ONE if, TWO loops only....
+ * @n: input
+ * Return: Alwasys 0 (Success)
+ */
+char *leet(char *n)
+{
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (n[i] == find[X])
+			{
+				n[i] = replacer[X / 2];
+				x = 9;
+			}
+		}
+	}
+}
