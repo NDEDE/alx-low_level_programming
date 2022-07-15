a522bf907aab3ef16d851b503e041a67ef345931
NDEDE <dennicendede@gmail.com>
2022-07-15 16:04:19 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-swap.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of int a and int b
+ * @a: first int to swap
+ * @b: second int toswap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
