ed2fcf3d2f7483e606acdb78b73490dc943174f5
NDEDE <dennicendede@gmail.com>
2022-07-15 16:21:42 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-puts.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * _puts - prints a string in stdout
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
