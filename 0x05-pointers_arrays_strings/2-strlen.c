30ca218550e4cb0492e774483a8f7b342f206852
NDEDE <dennicendede@gmail.com>
2022-07-15 16:13:16 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to find length of
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	return (i);
+}
