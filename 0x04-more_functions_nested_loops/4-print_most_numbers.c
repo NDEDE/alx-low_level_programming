c49f45cd28d0248a7dd37990603e247750ee9431
NDEDE <dennicendede@gmail.com>
2022-07-17 13:09:21 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 012356789\n
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
