80292c210fd90bd9f06be45c1839ec9cdef51bc0
NDEDE <dennicendede@gmail.com>
2022-07-17 09:57:51 -0700

    done with functions that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-mul.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: addition of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
