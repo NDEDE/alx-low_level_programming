2ba80d0453befc01b78ee7282b7cd4351e7775eb
NDEDE <dennicendede@gmail.com>
2022-07-18 03:57:25 -0700

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,9 @@
  * @dest: destination of copy
  * @src: memory area to copy
  * @n: number of bytes to copy
+ * Return: pointer to dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
