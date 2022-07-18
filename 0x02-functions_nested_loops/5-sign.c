325b35c7b2bd0577dd4a59bac39565f222041b07
NDEDE <dennicendede@gmail.com>
2022-07-17 18:09:16 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,22 +2,23 @@
  * File: 5-sign.c
  * Auth: Dennice Ndede
  */
-                   
+
 #include "main.h"
-                  
+
 /**
  * print_sign -> prints sign based on condition
  * @n: argument passed
  * Return: 1, 0 -1
  */
 int print_sign(int n)
-{                    
+{
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0);
+	else if
+		(n == 0);
 	{
 		_putchar(48);
 		return (0);
