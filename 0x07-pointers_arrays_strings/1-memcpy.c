d8a72ee08685506a4d73a1eabfff4f63e3e72ddc
NDEDE <dennicendede@gmail.com>
2022-07-18 03:50:17 -0700

    Done with function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination of copy
+ * @src: memory area to copy
+ * @n: number of bytes to copy
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
