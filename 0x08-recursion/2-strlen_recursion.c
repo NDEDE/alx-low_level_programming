6fa5977d7b20df2e36ce74a0d72324403846d4e0
NDEDE <dennicendede@gmail.com>
2022-07-20 18:27:39 -0700

    Prints the length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
