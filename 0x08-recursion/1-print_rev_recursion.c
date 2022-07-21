23723f31b4074d449c6c9967b7c2bce730cbe5eb
NDEDE <dennicendede@gmail.com>
2022-07-20 18:17:53 -0700

    Done with print in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,8 +8,9 @@
 /**
  * _print_rev_recursion - Prints a string in reverse, followed by a newline.
  * @s: the string to be printed
+ *
  */
-void _prints_rev_recusion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (s[0] != '\0')
 	{
