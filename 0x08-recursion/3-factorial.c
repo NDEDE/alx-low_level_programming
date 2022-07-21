48054ac35a055305c0735f24302f811ea3891e9c
NDEDE <dennicendede@gmail.com>
2022-07-20 18:40:02 -0700

    factorial numbers

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-factorial.c
+ * Auth: Dennice Ndede
+ */
+
+#include "main.h"
+
+/**
+ * factorial - Calculates factorial number.
+ * @n: number to calculate factorial.
+ * Return: factorial number
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	if (n > 1)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
