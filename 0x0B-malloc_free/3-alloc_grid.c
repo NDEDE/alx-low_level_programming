576ba3a77ad9d141921506632391c528db9cab9e
NDEDE <dennicendede@gmail.com>
2022-07-25 14:51:13 -0700

    mann

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,6 +10,7 @@
 /**
  *  alloc_grid - Entry point
  *@width: rows of matrix
+ **@height: columns of string
  *Return: a pointer to a 2 dimensional array of integers or null
  */
 int **alloc_grid(int width, int height)
